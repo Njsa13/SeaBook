109febe612a80ae66a643386405dea11e4de7602
Njsa13 <najib.sauqi@students.amikom.ac.id>
2023-07-10 20:36:56 +0700

    Menambahkan fitur login admin, fitur register & login user

---
+++ aksi_reg.php
@@ -0,0 +1,14 @@
+<?php
+include 'koneksi.php';
+$sql = sprintf(
+    "insert into user(nama, email, password) values ('%s', '%s', '%s')",
+    $_POST['nama'],
+    $_POST['email'],
+    $_POST['password'],
+);
+
+if (mysqli_query($link, $sql)) {
+    header('location:tampil_data.php');
+} else {
+    header('location:form_tambah.php');
+}
